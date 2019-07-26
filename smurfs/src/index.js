9c212153114519939775a8345481c703b7fd1e01
Jason Prince <jasoncprince@gmail.com>
2019-07-26 10:05:53 -0600

    provider and store setup in index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,17 @@
+// Dependancies
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+// Components
 import App from "./components/App";
+import reducer from "./store/reducers/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
